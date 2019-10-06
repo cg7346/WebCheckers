730a22cf406c5243b05fe4e6f255944176ddb35e
JacquelynLeung <jil4009@g.rit.edu>
2019-10-06 14:52:26 -0400

    added to boardview

--- src/main/java/com/webcheckers/ui/BoardView.java
+++ src/main/java/com/webcheckers/ui/BoardView.java
@@ -1,4 +1,63 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class BoardView {
+    private enum color {RED, WHITE}
+    private enum viewModel { PLAY, SPECTATOR, REPLAY }
+    private ArrayList<Row> rows;
+    private color activeColor;
+
+
+
+
+    public BoardView(Player player, CheckersGame checkersGame){
+        this.rows = new ArrayList<>();
+
+        CheckersGame.Space[][] board;
+        if(!checkersGame.isRedPlayer(player)){
+            //inverse items
+            board = checkersGame.inverseBoard();
+        } else {
+            board = checkersGame.getBoard();
+        }
+        ArrayList<Space> spaceList;
+
+        for(int i = 0; i < checkersGame.ROWS; i++){
+            spaceList = new ArrayList<>();
+            for(int j = 0; j < checkersGame.COLS; j++){
+                Space s;
+                switch (board[i][j]){
+                    case EMPTY_BLACK:
+                        s = new Space(j, true);
+                        break;
+                    case RED_PLAYER:
+                        s = new Space(j, true);
+                        s.addPiece(new Piece(0));
+                        break;
+                    case WHITE_PLAYER:
+                        s = new Space(j, true);
+                        s.addPiece(new Piece(1));
+                        break;
+                    default:
+                        s = new Space(j, false);
+                        break;
+                }
+                spaceList.add(s);
+            }
+            rows.add(new Row(i, spaceList));
+        }
+    }
+
+
+    /**
+     * @return the iterable list of rows containing an iterable list of spaces
+     */
+    public synchronized Iterator<Row> iterator() {
+        return rows.iterator();
+    }
 }
--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -16,14 +16,14 @@ public class Row {
      * if index is out of bounds then an index out of bounds exception will be called
      * @param index the row number
      */
-    public Row(int index, Space[] spaces){
+    public Row(int index, ArrayList<Space> spaces){
         if(index > 7 || index < 0){
             throw new IndexOutOfBoundsException();
         } else {
             this.index = index;
         }
         spaceList = new ArrayList<>();
-        spaceList.addAll(Arrays.asList(spaces));
+        spaceList.addAll(spaces);
 
     }
 
