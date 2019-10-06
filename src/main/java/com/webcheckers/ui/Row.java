84c460e2353e24ec61933cd6d4b284cc7861094a
JacquelynLeung <jil4009@g.rit.edu>
2019-10-06 12:21:34 -0400

    added in row items

--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -1,4 +1,17 @@
 package com.webcheckers.ui;
 
 public class Row {
+    private int index;
+
+    public Row(int index){
+        if(index > 7 || index < 0){
+            throw new IndexOutOfBoundsException();
+        } else {
+            this.index = index;
+        }
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
 }
