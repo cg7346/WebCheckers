4507cd871f614e9a170e3df01eb4fb6acf958a45
JacquelynLeung <jil4009@g.rit.edu>
2019-10-06 13:23:26 -0400

    row update

--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -1,5 +1,7 @@
 package com.webcheckers.ui;
 
+import java.util.*;
+
 /**
  * creates a row
  * @author Jacquelyn Leung
@@ -7,18 +9,22 @@ package com.webcheckers.ui;
 
 public class Row {
     private int index;
+    private ArrayList<Space> spaceList;
 
     /**
      * initialize row
      * if index is out of bounds then an index out of bounds exception will be called
      * @param index the row number
      */
-    public Row(int index){
+    public Row(int index, Space[] spaces){
         if(index > 7 || index < 0){
             throw new IndexOutOfBoundsException();
         } else {
             this.index = index;
         }
+        spaceList = new ArrayList<>();
+        spaceList.addAll(Arrays.asList(spaces));
+
     }
 
     /**
@@ -27,5 +33,14 @@ public class Row {
      */
     public int getIndex(){
         return this.index;
+    }
+
+    /**
+     * creates a java iterator of the spaces in the row
+     * @return Iterator of space
+     */
+    public Iterator<Space> iterator(){
+        return this.spaceList.listIterator();
+
     }
 }
