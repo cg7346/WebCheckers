767716a636ff9fa5c2ce6c493fce86a845201971
Kelly <kdv6978@rit.edu>
2019-10-06 11:30:35 -0400

    Created Player Model

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,5 +1,11 @@
 package com.webcheckers.appl;
 
+/**
+ * The object to coordinate all the Players.
+ *
+ *
+ * @author <a href='mailto:kdv6978@rit.edu'>Kelly Vo</a>
+ */
 public class PlayerLobby {
 
     public PlayerLobby() {
