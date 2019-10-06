b5c7f462b64b59b2db7fa70d29fffa01b90aa1f3
JacquelynLeung <jil4009@g.rit.edu>
2019-10-06 12:31:39 -0400

    added in comments

--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -3,6 +3,11 @@ package com.webcheckers.ui;
 public class Row {
     private int index;
 
+    /**
+     * initialize row
+     * if index is out of bounds then an index out of bounds exception will be called
+     * @param index the row number
+     */
     public Row(int index){
         if(index > 7 || index < 0){
             throw new IndexOutOfBoundsException();
@@ -11,6 +16,10 @@ public class Row {
         }
     }
 
+    /**
+     * This will return the index of the row
+     * @return row index
+     */
     public int getIndex(){
         return this.index;
     }
