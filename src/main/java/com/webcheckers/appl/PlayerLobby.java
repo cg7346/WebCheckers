4f772f290e95933a4c92ca204dff3a0ae4c952ea
cg7346 <cg7346@rit.edu>
2019-10-07 09:46:33 -0400

    Merge branch 'sign-in' of /Users/celestegambardella/OneDrive - rit.edu/SWEN 261/Project/team-project-b-2191-swen-261-11-b-incognito with conflicts.

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -8,7 +8,6 @@ import java.util.List;
 /**
  * The object to coordinate all the Players.
  *
- *
  * @author <a href='mailto:kdv6978@rit.edu'>Kelly Vo</a>
  * @author <a href='mailto:cg7346@rit.edu'>Celeste Gambardella</a>
  */
