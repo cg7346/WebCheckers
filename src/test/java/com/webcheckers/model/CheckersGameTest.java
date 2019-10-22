b12a56fd00340e15d6f1aa1a28392ab3f932893d
Anthony Ferraioli <amf7619@g.rit.edu>
2019-10-22 17:29:33 -0400

    Added tests to CheckersGameTest

--- src/test/java/com/webcheckers/model/CheckersGameTest.java
+++ src/test/java/com/webcheckers/model/CheckersGameTest.java
@@ -1,5 +1,6 @@
 package com.webcheckers.model;
 
+import com.webcheckers.ui.BoardView;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -23,37 +24,45 @@ class CheckersGameTest {
     }
 
     @Test
-    void isRedPlayer() {
+    void test_isRedPlayer() {
         assertEquals(true, CuT.isRedPlayer(p1), "P1 is supposed to be red player");
         assertNotEquals(true, CuT.isRedPlayer(p2), "P2 is not supposed to be red player");
     }
 
     @Test
-    void inverseBoard() {
+    void test_inverseBoard() {
         assertNotEquals(CuT.getBoard(), CuT.inverseBoard(), "The board is the same as the original");
     }
 
     @Test
-    void getBoard() {
+    void test_getBoard() {
         assertNotNull(CuT.getBoard(), "Board is null");
     }
 
     @Test
-    void getRedPlayer() {
+    void test_getRedPlayer() {
         assertNotNull(CuT.getRedPlayer(), "Red player is null");
         assertEquals(p1, CuT.getRedPlayer(), "Red player is not player 1");
         assertNotEquals(p2, CuT.getRedPlayer(), "Red player should not be player 2");
     }
 
     @Test
-    void getWhitePlayer() {
+    void test_getWhitePlayer() {
         assertNotNull(CuT.getWhitePlayer(), "White player is null");
         assertEquals(p2, CuT.getWhitePlayer(), "White player is not player 2");
         assertNotEquals(p1, CuT.getWhitePlayer(), "White player should not be player 1");
     }
 
     @Test
-    void getGameID() {
+    void test_getGameID() {
         assertEquals(ID, CuT.getGameID(), "Game ID is not equal to 0");
+    }
+
+    @Test
+    void test_getSpace()
+    {
+        CheckersGame.Space[][] board = CuT.getBoard();
+        char symbol = CuT.getSpace(0, 0);
+        assertEquals(CheckersGame.Space.EMPTY_WHITE.getSymbol(), symbol, "First symbol on board is not empty white");
     }
 }
--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -1,10 +1,19 @@
 package com.webcheckers.ui;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GetGameRouteTest {
+
+    GetGameRoute CuT;
+
+    @BeforeEach
+    void CreateGetGameRoute()
+    {
+        //CuT = new GetGameRoute();
+    }
 
     @Test
     void handle() {
