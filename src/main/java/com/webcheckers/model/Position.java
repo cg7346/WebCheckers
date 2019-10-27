682e0c61758892a6c7e2023cc12f4deae388f51d
JacquelynLeung <jil4009@g.rit.edu>
2019-10-27 16:07:43 -0400

    added in items to position. Pair programming with Mallory Bridge

--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -1,4 +1,21 @@
 package com.webcheckers.model;
 
+/**
+ * Position of the piece
+ * @author jil4009
+ * @author  mmb2582
+ */
 public class Position {
+
+    private int row;
+    private  int col;
+
+    public Position(int row, int col){
+        if(row <= 7 && row >= 0){
+            this.row = row;
+        }
+        if(col <= 7 && col >= 0) {
+            this.col = col;
+        }
+    }
 }
