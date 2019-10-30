53ecd11ca84a98f5964744f8474bef3ae2cf9d45
JacquelynLeung <jil4009@g.rit.edu>
2019-10-30 13:58:50 -0400

    added in post check turn

--- src/main/java/com/webcheckers/ui/PostCheckTurn.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurn.java
@@ -1,4 +1,45 @@
 package com.webcheckers.ui;
 
-public class PostCheckTurn {
+import com.google.gson.Gson;
+import com.webcheckers.appl.GameManager;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Move;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Objects;
+
+/**
+ * this checks for the turn of the user
+ * @author jil4009
+ */
+public class PostCheckTurn implements Route {
+    private CheckersGame game;
+    private Move move;
+    private final PlayerLobby playerLobby;
+    private final GameManager gameManager;
+    private final Gson gson;
+
+    public PostCheckTurn(PlayerLobby playerLobby, GameManager gameManager,Gson gson){
+        this.playerLobby = Objects.requireNonNull(playerLobby, "player lobby is required");
+        this.gameManager = Objects.requireNonNull(gameManager, "game manager is required");
+        this.gson = Objects.requireNonNull(gson, "gson is required");
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        String gameIdString = request.queryParams("gameID");
+        Player currentPlayer = playerLobby.getPlayer();
+        CheckersGame game = gameManager.getGame(Integer.parseInt(gameIdString));
+
+        if (game.getActivePlayer().equals(currentPlayer)) {
+            return gson.toJson(Message.info("true"));
+        } else {
+            return gson.toJson(Message.info("false"));
+        }
+    }
 }
