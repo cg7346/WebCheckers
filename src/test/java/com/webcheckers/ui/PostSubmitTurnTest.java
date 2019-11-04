bacbd7de52244f9d0d15b03e9d26fe18f9b1c76d
Mallory Bridge <43453083+mmb2582@users.noreply.github.com>
2019-11-03 23:51:10 -0500

    full coverage submitTurn

--- src/test/java/com/webcheckers/ui/PostSubmitTurnTest.java
+++ src/test/java/com/webcheckers/ui/PostSubmitTurnTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.webcheckers.appl.GameManager;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Move;
 import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -52,6 +53,18 @@ public class PostSubmitTurnTest {
     void test_lastMoveIsNull(){
         when(mockGame.getLastMove()).thenReturn(null);
         String expected = "{\"text\":\"Make move first\",\"type\":\"ERROR\"}";
+        try {
+            Object o = CuT.handle(request, response);
+            assertEquals(expected, o.toString());
+        }catch (Exception e){
+            //squahs
+        }
+    }
+
+    @Test
+    void test_validMove(){
+        when(mockGame.getLastMove()).thenReturn(mock(Move.class));
+        String expected = "{\"text\":\"Valid Move!\",\"type\":\"INFO\"}";
         try {
             Object o = CuT.handle(request, response);
             assertEquals(expected, o.toString());
