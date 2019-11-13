0ec953d35808e928fc8ea5e07650a311d9153dfa
cg7346 <cg7346@rit.edu>
2019-11-13 13:21:47 -0500

    created classes for spectator

---
+++ src/main/java/com/webcheckers/ui/GetSpectatorRoute.java
@@ -0,0 +1,10 @@
+package com.webcheckers.ui;
+
+/**
+ * The UI controller to GET the Spectator page
+ *
+ * @author <a href='mailto:cg7346@rit.edu'>Celeste Gambardella<a/>
+ * @author <a href='mailto:amf7619@rit.edu'>Anthony Ferraioli</a>
+ */
+public class GetSpectatorRoute {
+}
